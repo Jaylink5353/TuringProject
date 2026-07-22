e3400b63f7cb387deeffea70ee663c4c30408c60
Jaymes <jaylink5353@gmail.com>
2026-07-22 13:35:02 -0400

    fixes for pi

--- piMain.py
+++ piMain.py
@@ -15,6 +15,7 @@ print("Networking Up")
 
 serial_hw = piSerialLib()
 
+stop = True
 while True:
     time.sleep(0.01)
     serial_hw.checkHeartTime()
@@ -23,16 +24,18 @@ while True:
         command = cmd_queue.get_nowait()
         print(f"Processing incoming command: {command}")
         if command == "W":
-           piSerialLib.sendCommand(command="F", speed=spd)
+           serial_hw.sendCommand(command="F", speed=spd)
         elif command == "S":
-            piSerialLib.sendCommand(command="B", speed=spd)
+            serial_hw.sendCommand(command="B", speed=spd)
         elif command == "A":
-            piSerialLib.sendCommand(command="TL", speed=spd)
+            serial_hw.sendCommand(command="TL", speed=spd)
         elif command == "D":
-            piSerialLib.sendCommand(command="TR", speed=spd)
+            serial_hw.sendCommand(command="TR", speed=spd)
         elif command == "X":
-            piSerialLib.sendCommand(command="S", speed=0)
+            serial_hw.sendCommand(command="S", speed=0)
     except queue.Empty:
-        piSerialLib.sendCommand(command="S", speed="0")
+        if stop == False:
+            serial_hw.sendCommand(command="S", speed="0")
+            stop = True
         pass
         
