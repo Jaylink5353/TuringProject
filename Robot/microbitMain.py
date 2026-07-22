76bccbe805429f784b083de2db852157bed298d7
Jaymes <jaylink5353@gmail.com>
2026-07-22 14:00:31 -0400

    Update microbitMain.py

--- Robot/microbitMain.py
+++ Robot/microbitMain.py
@@ -1,19 +1,63 @@
 from microbit import *
 
 from piSerialLibmk3 import microPiSerial
-# from keyes_mecanum_car_v2 import *
+from keyes_mecanum_car_v2 import *
 
 serial = microPiSerial()
-# car = Mecanum_Car_Driver_V2()
+car = Mecanum_Car_Driver_V2()
+
+def move_forward(speed):
+    car.Motor_Upper_L(1, speed)
+    car.Motor_Lower_L(1, speed)
+    car.Motor_Upper_R(1, speed)
+    car.Motor_Lower_R(1, speed)
+
+def move_backward(speed):
+    car.Motor_Upper_L(0, speed)
+    car.Motor_Lower_L(0, speed)
+    car.Motor_Upper_R(0, speed)
+    car.Motor_Lower_R(0, speed)
 
 def stop():
-    return
+    car.Motor_Upper_L(0, 0)
+    car.Motor_Lower_L(0, 0)
+    car.Motor_Upper_R(0, 0)
+    car.Motor_Lower_R(0, 0)
+
+def move_left(speed):
+    car.Motor_Upper_L(1, speed)
+    car.Motor_Lower_L(0, speed)
+    car.Motor_Upper_R(0, speed)
+    car.Motor_Lower_R(1, speed)
+
+def move_right(speed):
+    car.Motor_Upper_L(0, speed)
+    car.Motor_Lower_L(1, speed)
+    car.Motor_Upper_R(1, speed)
+    car.Motor_Lower_R(0, speed)
+
+def turn_left(speed):
+    car.Motor_Upper_L(0, 0)
+    car.Motor_Lower_L(0, 0)
+    car.Motor_Upper_R(1, speed)
+    car.Motor_Lower_R(1, speed)
+
+def turn_right(speed):
+    car.Motor_Upper_L(1, speed)
+    car.Motor_Lower_L(1, speed)
+    car.Motor_Upper_R(0, 0)
+    car.Motor_Lower_R(0, 0)
 
 while True:
     packet = serial.readUart()
     if packet is not None:
         command, speed = packet
-        # put all commands here
+        if command == "F":
+            move_forward(speed)
+        if command == "X":
+            stop()
+        if command == "B":
+            move_backward(speed)
     if packet is None:
         if serial.checkHeartbeat() == False:
             stop()
