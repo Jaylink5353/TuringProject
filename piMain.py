d7208fae80d9f3e5e22adf27404d9b010c3aad16
Jaymes G <jaymes.5353@gmail.com>
2026-07-22 20:49:26 -0400

    cam server threading

--- piMain.py
+++ piMain.py
@@ -9,13 +9,20 @@ spd = 100
 
 cmd_queue = queue.Queue()
 
-camsrv = CamServer()
-camsrv.run(host='0.0.0.0', port=5000)
-
 socketServer = piSocketLib(command_queue=cmd_queue)
 server_thread = threading.Thread(target=socketServer.listen, daemon=True)
 server_thread.start()
 print("Networking Up")
+
+camsrv = CamServer()
+
+cam_thread = threading.Thread(
+    target=camsrv.run,
+    kwargs={'host': '0.0.0.0', 'port': 5000},
+    daemon=True
+)
+cam_thread.start()
+print("CamServer Up")
 
 serial_hw = piSerialLib()
 
