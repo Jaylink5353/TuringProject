be41b3fa24ea3d8270228cc38c778bb8640637bc
Jaymes G <jaymes.5353@gmail.com>
2026-07-22 20:48:11 -0400

    cam server 2

--- CamServer/camserver.py
+++ CamServer/camserver.py
@@ -8,6 +8,17 @@ class CamServer:
         self.cap = cv2.VideoCapture(0)
         self._register_routes()
 
+    class CamServer:
+        def __init__(self):
+            self.app = Flask(__name__)
+            self.cap = cv2.VideoCapture(0)
+            self._register_routes()
+
+        # Add this method to your class
+        def run(self, host="0.0.0.0", port=5000, **kwargs):
+            self.app.run(host=host, port=port, **kwargs)
+
+
     def _register_routes(self):
         self.app.add_url_rule("/", view_func=self.index)
         self.app.add_url_rule("/video_feed", view_func=self.video_feed)
