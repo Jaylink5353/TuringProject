105f595511e1363f81612673ee6f7c1a32291be4
Jaymes G <jaymes.5353@gmail.com>
2026-07-22 20:28:03 -0400

    Prepping work for cam server integration

--- piMain.py
+++ piMain.py
@@ -3,19 +3,19 @@ import time
 import threading
 from Networking.piSocket import piSocketLib
 from Serial.piSerialLib import piSerialLib
-# from CamServer.camserver import camServer
+from CamServer.camserver import CamServer
 
 spd = 100
 
 cmd_queue = queue.Queue()
-# camServer.run(host='0.0.0.0', port=5000)
+CamServer.run(host='0.0.0.0', port=5000)
 
 socketServer = piSocketLib(command_queue=cmd_queue)
 server_thread = threading.Thread(target=socketServer.listen, daemon=True)
 server_thread.start()
 print("Networking Up")
 
-serial_hw = piSerialLib()
+serial_hw = piSerialLib()1
 
 stop = True
 moving = False
