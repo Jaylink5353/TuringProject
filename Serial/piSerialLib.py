6f1976c4eb20d29cf9d0f4e17406963530390fd8
Jaymes <jaylink5353@gmail.com>
2026-07-28 20:58:49 -0400

    trying stuff to fix stop issues

--- Serial/piSerialLib.py
+++ Serial/piSerialLib.py
@@ -9,13 +9,18 @@ class piSerialLib:
 
     def sendCommand(self,command:str, speed:int):
         # for i in range(1):
-        packet = f"{command}:{speed};\n"
-        self.ser.reset_input_buffer()
-        self.ser.write(packet.encode('utf-8'))
-        print(f"Packet Sent: {packet}")
-        ack = self.waitForAck()
-        # if ack == True:
-            # break
+        if command == "S":
+            CmdRange = 3
+        else:
+            CmdRange = 1
+        for i in range (CmdRange):
+            packet = f"{command}:{speed};\n"
+            self.ser.reset_input_buffer()
+            self.ser.write(packet.encode('utf-8'))
+            print(f"Packet Sent: {packet}")
+            ack = self.waitForAck()
+            if ack == True:
+                break
 
 
 
